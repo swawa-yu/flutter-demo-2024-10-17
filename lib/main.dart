2dcf3906b9e32621eabea2393e0f4776ed7797ba
swawa-yu <swawa.yu@gmail.com>
2024-10-17 20:07:19 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TaskManagerApp());
+}
+
+class TaskManagerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'タスク管理アプリ',
+      home: TaskManagerHomePage(),
+    );
+  }
+}
+
+class TaskManagerHomePage extends StatefulWidget {
+  @override
+  _TaskManagerHomePageState createState() => _TaskManagerHomePageState();
+}
+
+class _TaskManagerHomePageState extends State<TaskManagerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('タスク管理'),
+      ),
+      body: ListView(children: <Widget>[
+        ListTile(
+            leading: Icon(Icons.check_box_outline_blank), title: Text('タスク1')),
+        ListTile(
+            leading: Icon(Icons.check_box_outline_blank), title: Text('タスク2')),
+      ]),
+    );
+  }
+}
